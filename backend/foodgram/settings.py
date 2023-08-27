0f50b7c54cae381584f615a4e505ef8bc5892875
plotnik-pr <tarronmorran@gmail.com>
2023-08-27 19:03:56 +0400

    Миграции

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -160,7 +160,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/collected_static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
@@ -171,5 +171,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PASSWORD_RESET_TIMEOUT = 60 * 60
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
