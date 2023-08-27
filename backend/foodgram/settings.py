99899429e9bc717aba5d5b23e9bab6af34315e07
plotnik-pr <tarronmorran@gmail.com>
2023-08-27 17:31:18 +0400

    Деплой 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -160,8 +160,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/collected_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
